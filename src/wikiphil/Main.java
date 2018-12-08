11c157e8c722306734cee1e87ae7713be8483a83
Gui Guy <jedandeganwang1999@gmail.com>
2018-12-07 18:52:26 -0600

    debugging big IQ not

--- src/wikiphil/Main.java
+++ src/wikiphil/Main.java
@@ -40,18 +40,18 @@ public class Main {
                 }
             }).start();
         }*/
-        /*for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < 100; i++) {
             double start = System.nanoTime();
             Document tempD = Jsoup.connect(
                     "https://en.wikipedia.org/wiki/Special:Random").get();
             String temp = tempD.selectFirst("h1#firstHeading").text() + 
-                    ": " + hopsToPhilosophy(
+                    ": " + forceToPhilosophy(
                     tempD.location()) + " hops";
             System.out.printf("%-100s", temp);
             double total = System.nanoTime() - start;
             System.out.printf("%.4fms%n", total/1000000);
-        }*/
-        traceToPhilosophy("https://en.wikipedia.org/wiki/State_governments_of_India");
+        }
+        // traceToPhilosophy("https://en.wikipedia.org/wiki/State_governments_of_India");
     }
     
     /**
@@ -177,7 +177,7 @@ public class Main {
                                 && !linkHref.contains("redlink") && 
                                 !linkHref.contains("upload.wikimedia.org")) {
                             toGo = link;
-                            System.out.println(link.parents().select("div"));
+                            System.out.println(link.parents());
                             break outer;
                         }
                     }
@@ -203,7 +203,10 @@ public class Main {
                                 && !linkHref.contains("redlink") &&
                                 !linkHref.contains("upload.wikimedia.org")) {
                             toGo = link;
-                            System.out.println(link.parents());
+                            for(Element parent : toGo.parents()) {
+                                System.out.print(parent.nodeName() + " ");
+                            }
+                            System.out.println();
                             break outer;
                         }
                     }
@@ -215,6 +218,103 @@ public class Main {
             current = Jsoup.connect(
                     "https://en.wikipedia.org" + toGo.attr("href")).get();
         }
+    }
+    
+    /**
+     * Forces a link pathway to Philosophy
+     * @param toConnect the page to start at
+     * @return the number of steps on the forced pathway
+     * @throws IOException if something goes wrong
+     */
+    public static int forceToPhilosophy(String toConnect) throws IOException {
+        return forceToPhilosophy(toConnect, new HashSet<>(), 0);
+    }
+    
+    /**
+     * Forces a link pathway to Philosophy
+     * @param toConnect the page to start at
+     * @param visited a HashSet of pages that have been already visited
+     * @param hops the number of current hops already taken
+     * @return the number of steps on the forced pathway
+     * @throws IOException if something goes wrong
+     */
+    private static int forceToPhilosophy(String toConnect, 
+            HashSet<String> visited, int hops) throws IOException {
+        /*Document current = Jsoup.parse(parse(Jsoup.connect(
+                    "https://en.wikipedia.org/wiki/Special:Random")
+                .get().outerHtml()));*/
+        Document current = Jsoup.connect(
+                toConnect).get();
+        if(!current.title().equals("Philosophy - Wikipedia")) {
+            String title = current.selectFirst("h1#firstHeading").text();
+            // System.out.println(title);
+            if(!visited.add(title)) {
+                /*if(title.equals("Existence") || title.equals("Reality"))
+                    return -2;*/
+                return -hops-1;
+            }
+            Elements bodyStuff = current.select("div#bodyContent");
+            Elements parags = bodyStuff.select("p");
+            if(parags.select("a[href]").isEmpty()) {
+                Elements listElements = bodyStuff.select("li");
+                outer: for(Element listElement: listElements) {
+                    String temp = listElement.outerHtml();
+                    TreeMap<Integer, Integer> map = map(temp);
+                    Document li = Jsoup.parse(temp);
+                    Elements links = li.select("a[href]");
+                    if(links.isEmpty()) continue;
+                    for(Element link : links) {
+                        if(link.parent().is("span#coordinates")) 
+                            continue outer;
+                        String outer = link.outerHtml();
+                        if(map.get(map.floorKey(temp.indexOf(outer))) != 0) 
+                            continue;
+                        String linkHref = link.attr("href");
+                        if(!linkHref.contains("#") && !linkHref.contains(":")
+                                && !linkHref.contains("action=edit") && 
+                                !linkHref.contains("upload.wikimedia.org")) {
+                            // try this one
+                            int force = forceToPhilosophy(
+                                    "https://en.wikipedia.org" 
+                                            + link.attr("href"), 
+                                    new HashSet<>(visited), hops+1);
+                            if(force >= 0) return force;
+                        }
+                    }
+                }
+            } else {
+                outer: for (Element parag : parags) {
+                    if(parag.parent().tagName().equals("td")) 
+                        continue;
+                    String temp = parag.outerHtml();
+                    TreeMap<Integer, Integer> map = map(temp);
+                    Document p = Jsoup.parse(temp);
+                    Elements links = p.select("a[href]");
+                    if(links.isEmpty()) continue;
+                    for(Element link : links) {
+                        if(link.parent().is("span#coordinates")) 
+                            continue outer;
+                        String outer = link.outerHtml();
+                        if(map.get(map.floorKey(temp.indexOf(outer))) != 0) 
+                            continue;
+                        String linkHref = link.attr("href");
+                        if(!"".equals(linkHref) &&
+                                !linkHref.contains("#") && !linkHref.contains(":") 
+                                && !linkHref.contains("action=edit") &&
+                                !linkHref.contains("upload.wikimedia.org")) {
+                            // try this one
+                            int force = forceToPhilosophy(
+                                    "https://en.wikipedia.org" 
+                                            + link.attr("href"), 
+                                    new HashSet<>(visited), hops+1);
+                            if(force >= 0) return force;
+                        }
+                    }
+                }
+            }
+            return -hops-1;
+        }
+        return hops;
     }
     
     /**
@@ -284,7 +384,7 @@ public class Main {
         String copy = document;
         TreeMap<Integer, Integer> parentheses = new TreeMap<>();
         parentheses.put(0, 0);
-        int open, close, add = 0;
+        int open, close, add = 0, in = 0;
         for(;;) {
             open = copy.indexOf("("); 
             close = copy.indexOf(")");
@@ -295,8 +395,10 @@ public class Main {
                 } else {
                     // only closing
                     // temp = close;
-                    parentheses.put(add + close, 
-                            parentheses.get(parentheses.lastKey()) - 1);
+                    if(in > 0) {
+                        parentheses.put(add + close,
+                                --in);
+                    }
                     close++;
                     add += close;
                     copy = copy.substring(close);
@@ -306,7 +408,7 @@ public class Main {
                     // only opening
                     // temp = open;
                     parentheses.put(add + open, 
-                            parentheses.get(parentheses.lastKey()) + 1);
+                            ++in);
                     open++;
                     add += open;
                     copy = copy.substring(open);
@@ -314,13 +416,15 @@ public class Main {
                     // both
                     if(open < close) {
                         parentheses.put(add + open, 
-                                parentheses.get(parentheses.lastKey()) + 1);
+                                ++in);
                         open++;
                         add += open;
                         copy = copy.substring(open);
                     } else {
-                        parentheses.put(add + close, 
-                                parentheses.get(parentheses.lastKey()) - 1);
+                        if(in > 0) {
+                            parentheses.put(add + close,
+                                    --in);
+                        }
                         close++;
                         add += close;
                         copy = copy.substring(close);
